91bcdc75ebc9d23e43437640b22a4be34fb77ad8
Tan20114 <156587199+Tan20114@users.noreply.github.com>
2025-01-24 22:30:22 +0700

    Transition

---
+++ Assets/Scenes/Tantan/Script/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    SceneLoader sl;
+
+    [SerializeField] Button startButt;
+    [SerializeField] Button exitButt;
+
+    void Start()
+    {
+        sl = FindAnyObjectByType<SceneLoader>();
+
+        startButt.onClick.AddListener(StartGame);
+        exitButt.onClick.AddListener(ExitGame);
+    }
+
+    void StartGame()
+    {
+        StartCoroutine(sl.LoadScene(0));
+    }
+
+
+    void ExitGame()
+    {
+        StartCoroutine(sl.ExitGame());
+    }
+}
---
+++ Assets/Scenes/Tantan/Script/SceneLoader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Animator transAnimator;
+    [SerializeField] float animTime = 1f;
+
+    private void Awake()
+    {
+        transAnimator.SetInteger("SceneValue", 0/*SceneManager.GetActiveScene().buildIndex*/);
+    }
+
+    public IEnumerator LoadScene(int index)
+    {
+        transAnimator.SetTrigger("SceneOut");
+        yield return new WaitForSecondsRealtime(animTime);
+        SceneManager.LoadScene(index);
+    }
+
+    public IEnumerator ExitGame()
+    {
+        transAnimator.SetTrigger("Exit");
+        yield return new WaitForSecondsRealtime(animTime);
+        Application.Quit();
+    }
+}
